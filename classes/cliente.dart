ad4596471c9364eb8989d2b0eef8914b0bd82261
Franciéllen Sousa Araújo <franciellensousaaraujo@gmail.com>
2024-04-10 18:33:08 -0300

    fix: Corrigir método média de valores dos produtos

--- classes/cliente.dart
+++ classes/cliente.dart
@@ -4,7 +4,6 @@ import 'produto.dart';
 import 'revendedor.dart';
 
 class Cliente extends Pessoa {
-
   double dinheiro;
 
   List<Produto> produtosComprados = [];
@@ -16,7 +15,6 @@ class Cliente extends Pessoa {
     required super.genero,
     this.dinheiro = 0.0,
   });
-
 
   //método falar
   @override
@@ -35,33 +33,30 @@ class Cliente extends Pessoa {
           '$nome não possui dinheiro suficiente para comprar o produto ${produto.nome}');
     }
   }
-  
-    void adicionarDinheiro(double? valor) {
+
+  void adicionarDinheiro(double? valor) {
     if (valor != null) {
       dinheiro += valor;
-      print("$nome adicionou $valor à sua carteira. Agora você tem $dinheiro em dinheiro.");
+      print(
+          "$nome adicionou $valor à sua carteira. Agora você tem $dinheiro em dinheiro.");
     } else {
       print("Nenhum valor adicionado à carteira");
     }
   }
 
   void calcularMediaValorProdutosComprados() {
-    if(produtosComprados.isEmpty) {
+    if (produtosComprados.isEmpty) {
       print("Cliente $nome não possui produtos comprados");
     } else {
       List<Produto> produtosComprados = this.produtosComprados;
-      double valorTotalGasto = 0.0;
       int numeroDeProdutos = produtosComprados.length;
       double valorMedio;
 
-      produtosComprados.forEach((produto) {
-        valorTotalGasto += produto.valor;
-      });
-
+      double valorTotalGasto = calcularTotalGasto();
       valorMedio = valorTotalGasto / numeroDeProdutos;
 
-      print("O valor médio gasto em produtos pelo cliente $nome, é de R\$ $valorMedio");
+      print(
+          "O valor médio gasto em produtos pelo cliente $nome, é de R\$ $valorMedio");
     }
   }
 }
-
