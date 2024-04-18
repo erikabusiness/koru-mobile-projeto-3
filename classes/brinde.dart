663fccdb6892414edd8df268f0936c07ce7f0192
Jhonny <vsjhonny@gmail.com>
2024-04-18 14:17:15 -0300

    feat: Criado o atributo de quantidade em estoque.

--- classes/brinde.dart
+++ classes/brinde.dart
@@ -1,5 +1,7 @@
-
 class Brinde {
+  int qtdEmEstoque = 0;
+
+  Brinde({required this.qtdEmEstoque});
 
   void consultarPontosNecessarios() {
     print('Para ganhar um(a) $nome você precisa de $pontosNecessarios pontos');
