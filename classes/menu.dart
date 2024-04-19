38b2fb737796e2467223ddb8090c08cfb52990df
C1337001 Emerson Mendes da Costa <c1337001@interno.bb.com.br>
2024-04-18 21:50:24 -0300

    - feat: criação do menu de interação do usuario no terminal

--- classes/menu.dart
+++ classes/menu.dart
@@ -1,156 +1,186 @@
 import 'dart:io';
 
+import '../utils.dart';
 import 'cliente.dart';
 import 'enums.dart';
 import 'produto.dart';
 import 'revendedor.dart';
 
-main() {
+class Menu {
+  static String prompt(String promptText) {
+    stdout.write(promptText);
+    return stdin.readLineSync()!;
+  }
 
+  static bool confirmar(String mensagem) {
+    String resposta = prompt(mensagem + " (S/N): ");
+    pularLinha();
+    return resposta.toLowerCase() == 's';
+  }
+
+  static Cliente criarNovoCliente() {
+    late String nomeNovoCliente;
+    late String cpfNovoCliente;
+    late DateTime dataNascimentoNovoCliente;
+    late Genero generoNovoCliente;
+
+    do {
+      nomeNovoCliente = prompt("Olá, por favor digite seu nome: ");
+    } while (!confirmar("Seu nome [$nomeNovoCliente] está correto?"));
+
+    do {
+      cpfNovoCliente = prompt("$nomeNovoCliente, Digite seu CPF: ");
+    } while (!confirmar("Seu CPF [$cpfNovoCliente] está correto?"));
+
+    do {
+      int dia = int.parse(prompt("$nomeNovoCliente, digite seu dia de nascimento: "));
+      int mes = int.parse(prompt("$nomeNovoCliente, agora digite seu mês de nascimento: "));
+      int ano = int.parse(prompt("$nomeNovoCliente, por último digite seu ano de nascimento: "));
+      dataNascimentoNovoCliente = DateTime(ano, mes, dia);
+    } while (!confirmar("Sua data de nascimento [${formatarData(dataNascimentoNovoCliente)}] está correta?"));
+
+    bool generoConfirmado = false;
+    do {
+      String entradaGenero = prompt("$nomeNovoCliente, com qual gênero você se identifica? (M - Masculino / F - Feminino / O - Outro): ");
+      switch (entradaGenero.toLowerCase()) {
+        case 'm':
+          generoNovoCliente = Genero.Masculino;
+          break;
+        case 'f':
+          generoNovoCliente = Genero.Feminino;
+          break;
+        case 'o':
+          generoNovoCliente = Genero.Outro;
+          break;
+        default:
+          stdout.write("Opção inválida. Por favor, selecione novamente.\n");
+          continue;
+      }
+      if (confirmar("Seu gênero é ${generoNovoCliente.name}?")) {
+        generoConfirmado = true;
+      }
+    } while (!generoConfirmado);
+
+    return Cliente(
+      nome: nomeNovoCliente,
+      cpf: cpfNovoCliente,
+      dataDeNascimento: dataNascimentoNovoCliente,
+      genero: generoNovoCliente,
+    );
+  }
+
+  static void menuVenda(List<Revendedor> revendedores, List<Produto> produtos, Cliente cliente) {
+    pularLinha();
+    stdout.write("Olá ${cliente.nome}, bem-vindo(a) à central de compras. Abaixo estão nossos revendedores disponíveis para lhe atender no momento: \n");
+
+    revendedores.asMap().forEach((index, revendedor) {
+      stdout.write("[${index + 1}] - ${revendedor.nome}\n");
+    });
+
+    int? indiceSelecionado;
+    do {
+      int? entrada = int.tryParse(prompt("Selecione o número do revendedor para lhe atender: "));
+      if (entrada != null && entrada >= 1 && entrada <= revendedores.length) {
+        indiceSelecionado = entrada - 1;
+      } else {
+        stdout.write("Opção inválida. Por favor, selecione novamente.\n");
+      }
+    } while (indiceSelecionado == null);
+
+    Revendedor revendedorSelecionado = revendedores[indiceSelecionado];
+    stdout.write("Você selecionou ${revendedorSelecionado.nome} como seu revendedor.\nNosso catálogo de produtos disponíveis será exibido abaixo: \n");
+
+    produtos.asMap().forEach((index, produto) {
+      stdout.write("[${index + 1}] - ${produto.nome} - R\$${produto.valor}\n");
+    });
+
+    int? indiceProdutoSelecionado;
+    do {
+      int? entrada = int.tryParse(prompt("Selecione o número do produto que deseja comprar: "));
+      if (entrada != null && entrada >= 1 && entrada <= produtos.length) {
+        indiceProdutoSelecionado = entrada - 1;
+      } else {
+        stdout.write("Opção inválida. Por favor, selecione novamente.\n");
+      }
+    } while (indiceProdutoSelecionado == null);
+
+    Produto produtoSelecionado = produtos[indiceProdutoSelecionado];
+    stdout.write("Você selecionou o produto: ${produtoSelecionado.nome} - R\$${produtoSelecionado.valor}\n");
+
+    bool confirmacaoProduto = confirmar("Deseja confirmar a compra deste produto?");
+    if (confirmacaoProduto) {
+      cliente.comprarProduto(produtoSelecionado, revendedorSelecionado);
+    } else {
+      stdout.write("Compra cancelada.\n");
+    }
+  }
+
+  static void menuSaldo(Cliente cliente) {
+    pularLinha();
+
+    double? recarga;
+    bool confirmado = false;
+
+    do {
+      recarga = double.tryParse(prompt("Bem-vindo ao menu de recargas. Por favor, informe o valor que deseja adicionar à sua conta ou digite [0] para sair: "));
+
+      if (recarga == null || recarga < 0) {
+        stdout.write("Valor de recarga inválido. Por favor, insira um valor válido.\n");
+      } else if (recarga == 0) {
+        stdout.write("Saindo do menu de recargas.\n");
+        return;
+      } else {
+        stdout.write("Você está adicionando R\$$recarga à sua conta.\n");
+        confirmado = confirmar("Confirma o valor de recarga?");
+      }
+    } while (!confirmado);
+
+    cliente.adicionarDinheiro(recarga);
+    stdout.write("Saldo adicionado com sucesso! Novo saldo: R\$${cliente.dinheiro}\n");
+  }
+
+  static void menuPrincipal(List<Revendedor> revendedores, List<Produto> produtos) {
+    Cliente cliente = criarNovoCliente();
+    bool sair = false;
+
+    do {
+      int? entrada = int.tryParse(prompt("[1] - Comprar Produtos\n[2] - Adicionar Dinheiro\n[3] - Consultar Saldo\n[4] - Troca de Pontos\n[0] - Sair\n\n"
+          "Seja bem-vindo(a) ${cliente.nome}, ao nosso sistema de atendimento, escolha uma de nossas opções de atendimento: "));
+
+      switch(entrada) {
+        case 1:
+          menuVenda(revendedores, produtos, cliente);
+          break;
+        case 2:
+          menuSaldo(cliente);
+          break;
+        case 3:
+        stdout.write("O Seu saldo atual é de: ${cliente.dinheiro}\n");
+          break;
+        case 4:
+        //Chamar Menu de adição de valores
+          break;
+        case 0:
+          sair = true;
+          break;
+        default:
+          stdout.write("Opção [$entrada] invalida!");
+          continue;
+      }
+    } while (!sair);
+  }
+}
+
+void main() {
   List<Produto> catalogo = [
-    Produto(
-        nome: "Colônia Floratta Flores Secretas 75ml",
-        valor: 104.90,
-        qtdEmEstoque: 3
-    ),
+    Produto(nome: "Colônia Floratta Flores Secretas 75ml", valor: 104.90, qtdEmEstoque: 3),
     Produto(nome: "Colônia Floratta Red 75ml", valor: 75.90, qtdEmEstoque: 10),
   ];
 
-  List <Revendedor> revendedores = [
-    Revendedor(
-      nome: 'Ariel',
-      cpf: '12345678910',
-      dataDeNascimento: DateTime(1980, 6, 10),
-      genero: Genero.Outro,
-      matricula: '',
-    ),
-    Revendedor(
-        nome: 'jhonny',
-        cpf: '123456',
-        dataDeNascimento: DateTime.parse('1991-12-21'),
-        matricula: '123456789',
-        genero: Genero.Masculino),
+  List<Revendedor> revendedores = [
+    Revendedor(nome: 'Ariel', cpf: '12345678910', dataDeNascimento: DateTime(1980, 6, 10), genero: Genero.Outro, matricula: ''),
+    Revendedor(nome: 'jhonny', cpf: '123456', dataDeNascimento: DateTime.parse('1991-12-21'), matricula: '123456789', genero: Genero.Masculino),
   ];
 
-  menuPrincipal(revendedores, catalogo);
-}
-
-Cliente menuCriacaoNovoCliente() {
-  String? nomeNovoCliente = "";
-  String? cpfNovoCliente = "";
-  DateTime dataNascimentoNovoCliente = DateTime.now();
-  Genero generoNovoCliente = Genero.Outro;
-
-  bool nomeConfirmado = false;
-  bool cpfConfirmado = false;
-  bool dataNascimentoConfirmada = false;
-  bool generoConfirmado = false;
-
-  while (!nomeConfirmado) {
-    stdout.write("Olá, por favor digite seu nome: ");
-    String? entradaNome = stdin.readLineSync();
-
-    stdout.write(
-        "O nome $entradaNome está correto ? \nDigite:\n[1] - Sim\n[2] - Não\n");
-    String? resposta = stdin.readLineSync();
-    switch (resposta) {
-      case "1":
-        nomeNovoCliente = entradaNome;
-        nomeConfirmado = true;
-    }
-  }
-
-  while (!cpfConfirmado) {
-    stdout.write("$nomeNovoCliente, digite seu CPF: ");
-    String? entradaCpf = stdin.readLineSync();
-
-    stdout.write(
-        "O CPF $entradaCpf está correto ? \nDigite: \n[1] - Sim \n[2] - Não \n");
-    String? resposta = stdin.readLineSync();
-    switch (resposta) {
-      case "1":
-        cpfNovoCliente = entradaCpf;
-        cpfConfirmado = true;
-    }
-  }
-
-  while (!dataNascimentoConfirmada) {
-    stdout.write("$nomeNovoCliente, digite seu dia de nascimento: ");
-    String? diaNascimento = stdin.readLineSync();
-    stdout.write("$nomeNovoCliente, agora digite seu mês de nascimento: ");
-    String? mesNascimento = stdin.readLineSync();
-    stdout.write("$nomeNovoCliente, por ultimo digite seu ano de nascimento: ");
-    String? anoNascimento = stdin.readLineSync();
-
-    stdout.write(
-        "Sua data de nascimento $diaNascimento/$mesNascimento/$anoNascimento está correto ? \nDigite: \n[1] - Sim \n[2] - Não \n");
-    String? resposta = stdin.readLineSync();
-    switch (resposta) {
-      case "1":
-        dataNascimentoNovoCliente =
-            DateTime.parse("$anoNascimento-$mesNascimento-$diaNascimento");
-        dataNascimentoConfirmada = true;
-    }
-  }
-
-  while (!generoConfirmado) {
-    stdout.write(
-        "$nomeNovoCliente, Com qual genero você se identifica: \nDigite: \n[1] - Masculino \n[2] - Feminino \n[3] - Outro \n");
-    String? entradaGenero = stdin.readLineSync();
-
-    switch (entradaGenero) {
-      case "1":
-        generoNovoCliente = Genero.Masculino;
-        break;
-      case "2":
-        generoNovoCliente = Genero.Feminino;
-        break;
-      case "3":
-        generoNovoCliente = Genero.Outro;
-        break;
-      default:
-        continue;
-    }
-
-    stdout.write(
-        "Seu genero ${generoNovoCliente.name} está correto ? \nDigite: \n[1] - Sim \n[2] - Não \n");
-    String? resposta = stdin.readLineSync();
-    switch (resposta) {
-      case "1":
-        generoConfirmado = true;
-    }
-  }
-  Cliente novoCliente = Cliente(
-      nome: nomeNovoCliente!,
-      cpf: cpfNovoCliente!,
-      dataDeNascimento: dataNascimentoNovoCliente,
-      genero: generoNovoCliente
-  );
-
-  return novoCliente;
-}
-
-void menuPrincipal(List<Revendedor> revendedores, List<Produto> produtos) {
-  Cliente cliente = menuCriacaoNovoCliente();
-  late Revendedor revendedorSelecionado;
-
-  bool revendedorConfirmado = false;
-
-  stdout.write(
-      "Seja bem-vindo(a) ${cliente.nome}, escolha um(a) de nossos(as) revendedores(as) para lhe atender: \n");
-
-  revendedores.asMap().forEach((index, revendedor) {
-    stdout.write("[${index + 1}] - ${revendedor.nome}\n");
-  });
-
-  stdout.write("Selecione o revendedor para lhe atender");
-  String? entrada = stdin.readLineSync();
-
-  while(!revendedorConfirmado) {
-
-
-
-  }
-
-
+  Menu.menuPrincipal(revendedores, catalogo);
 }
