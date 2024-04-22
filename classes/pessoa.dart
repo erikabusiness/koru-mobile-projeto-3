4299ea76b3992cd4ff27cbcd757c414ed566b74c
Erika Alves <erikaalves.m.silva@gmail.com>
2024-04-22 09:39:58 -0300

    refactor: trocando aspas duplas por simples e adicionando o 'this'

--- classes/pessoa.dart
+++ classes/pessoa.dart
@@ -7,7 +7,6 @@ class Pessoa {
   final Genero genero;
   late int _idade;
 
-  //construtor
   Pessoa(
       {required this.nome,
       required this.cpf,
@@ -15,36 +14,36 @@ class Pessoa {
       required this.genero}) {
     _idade = _calcularIdade();
   }
-  //inserir metodo calcularIdade (retorno: int)
+
   int _calcularIdade() {
     return DateTime.now().difference(dataDeNascimento).inDays ~/ 365.toInt();
   }
 
   void verificarMaioridade() {
     String verificar = (_idade >= 18
-        ? ("$nome tem $_idade, portanto é maior de idade!")
-        : ("$nome tem $_idade, portanto é menor de idade!"));
+        ? ('${this.nome} tem ${this._idade} anos, portanto é maior de idade!')
+        : ('${this.nome} tem ${this._idade} anos, portanto é menor de idade!'));
     print(verificar);
   }
 
-  //método falar
   void falar(String fala) {
-    print('$nome diz: $fala');
+    print('${this.nome} diz: $fala');
   }
 
   void termometroDoHumor(Humor humor) {
     switch (humor) {
       case Humor.feliz:
-        print("Que bom que você está feliz hoje, $nome.");
+        print('Que bom que você está feliz hoje, ${this.nome}.');
         break;
       case Humor.alegre:
-        print("Que bom que você está alegre hoje, continue assim, $nome.");
+        print(
+            'Que bom que você está alegre hoje, continue assim, ${this.nome}.');
         break;
       case Humor.confiante:
-        print("Isso ai, graças a sua confiança ninguém pode te parar $nome!");
+        print('Isso ai, graças a sua confiança ninguém pode te parar ${this.nome}!');
         break;
       case Humor.triste:
-        print("Pare um pouco e respire, $nome.");
+        print('Pare um pouco e respire, ${this.nome}.');
         break;
     }
   }
