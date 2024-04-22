74cccdf4917686c9609d85e608096d16139296b8
Erika Alves <erikaalves.m.silva@gmail.com>
2024-04-22 09:45:34 -0300

    refactor: encapsulando a idade

--- classes/pessoa.dart
+++ classes/pessoa.dart
@@ -5,24 +5,21 @@ class Pessoa {
   final String cpf;
   final DateTime dataDeNascimento;
   final Genero genero;
-  late int _idade;
 
   Pessoa(
       {required this.nome,
       required this.cpf,
       required this.dataDeNascimento,
-      required this.genero}) {
-    _idade = _calcularIdade();
-  }
+      required this.genero});
 
-  int _calcularIdade() {
+  int get idade {
     return DateTime.now().difference(dataDeNascimento).inDays ~/ 365.toInt();
   }
 
   void verificarMaioridade() {
-    String verificar = (_idade >= 18
-        ? ('${this.nome} tem ${this._idade} anos, portanto é maior de idade!')
-        : ('${this.nome} tem ${this._idade} anos, portanto é menor de idade!'));
+    String verificar = (idade >= 18
+        ? ('${this.nome} tem ${this.idade} anos, portanto é maior de idade!')
+        : ('${this.nome} tem ${this.idade} anos, portanto é menor de idade!'));
     print(verificar);
   }
 
@@ -40,7 +37,8 @@ class Pessoa {
             'Que bom que você está alegre hoje, continue assim, ${this.nome}.');
         break;
       case Humor.confiante:
-        print('Isso ai, graças a sua confiança ninguém pode te parar ${this.nome}!');
+        print(
+            'Isso ai, graças a sua confiança ninguém pode te parar ${this.nome}!');
         break;
       case Humor.triste:
         print('Pare um pouco e respire, ${this.nome}.');
